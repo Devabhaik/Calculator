6ae24cab1b5a401540f3c2809c6921b19fc55149
Devabhaik <dbhaik1991@gmail.com>
2021-12-31 17:46:00 +0530

    input function

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,55 +1,64 @@
 import React, { Component, useState } from 'react';
 import './Calculator.css';
 
-const Number = [
-  1,
-  2,
-  3,
-  4,
-  5,
-  6,
-  7,
-  8,
-  9,
-  0,
-  '+',
-  '-',
-  '*',
-  '/',
-  '%',
-  '.',
-  '<',
-  '=',
-];
+const Numberinput = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '00', '000'];
+const Operator = ['+', '-', '*', '/', '%', '.', '<', '='];
 
 class Calculator extends Component {
   constructor(props) {
     super(props);
-    this.state = { Num: 0 };
+    this.state = {
+      Result: 0,
+      Firstnum: [],
+    };
 
     this.Compute = (x) => {
-      this.setState({ Num: x.target.innerText });
-      console.log(x.target.innerText);
+      this.state.Firstnum.push(x.target.innerText);
+
+      let NewNum = this.state.Firstnum.map((x) => {
+        return parseInt(x);
+      });
+
+      this.setState({ Result: NewNum });
+      console.log(NewNum);
+
+      if (x.target.innerText == '<') {
+        this.setState({ Result: 0 });
+      }
     };
   }
   render() {
-    console.log(Number);
     return (
       <div className="container">
         <div className="calculator">
-          <input type="number" value={this.state.Num} onChange={this.Compute} />
-          <div className="input_btn">
-            {Number.map((x) => {
-              return (
-                <button
-                  id={`input${x}`}
-                  key={`input${x}`}
-                  onClick={this.Compute}
-                >
-                  {x}
-                </button>
-              );
-            })}
+          <input value={this.state.Result} onChange={this.Compute} />
+          <div className="btn">
+            <div className="input_btn">
+              {Numberinput.map((x) => {
+                return (
+                  <button
+                    id={`input${x}`}
+                    key={`input${x}`}
+                    onClick={this.Compute}
+                  >
+                    {x}
+                  </button>
+                );
+              })}
+            </div>
+            <div className="operate_btn">
+              {Operator.map((x) => {
+                return (
+                  <button
+                    id={`input${x}`}
+                    key={`input${x}`}
+                    onClick={this.Compute}
+                  >
+                    {x}
+                  </button>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
