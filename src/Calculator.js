a8a4309a605b6fa7ae454d21717abf110100d1e8
Devabhaik <dbhaik1991@gmail.com>
2021-12-31 11:54:59 +0530

    input field set

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import './Calculator.css';
 
 const Number = [
@@ -18,20 +18,34 @@ const Number = [
   '/',
   '%',
   '.',
+  '<',
   '=',
 ];
 
 class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { Num: 0 };
+
+    this.Compute = (x) => {
+      this.setState({ Num: x.target.innerText });
+      console.log(x.target.innerText);
+    };
+  }
   render() {
     console.log(Number);
     return (
       <div className="container">
         <div className="calculator">
-          <input type="number" defaultValue="0" />
+          <input type="number" value={this.state.Num} onChange={this.Compute} />
           <div className="input_btn">
             {Number.map((x) => {
               return (
-                <button id={`input${x}`} key={`input${x}`}>
+                <button
+                  id={`input${x}`}
+                  key={`input${x}`}
+                  onClick={this.Compute}
+                >
                   {x}
                 </button>
               );
